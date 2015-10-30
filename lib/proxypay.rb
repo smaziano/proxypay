de57031aacd61dc95b1eca46d2738bcb7621ad7a
Sergio Maziano <sergio.maziano@gmail.com>
2015-10-30 18:37:22 +0100

    Refactored the authentication to a private method and added the support for custom_fields to the new reference request.

--- lib/proxypay.rb
+++ lib/proxypay.rb
@@ -7,30 +7,30 @@ module Proxypay
 
   # Fetch all available references
   def self.get_references(options={})
-    auth = {
-      username:ENV["PROXY_PAY_USER"],
-      password:ENV["PROXY_PAY_PASSWORD"]
-    }
-    options = {basic_auth:auth}
+    options = {:basic_auth => authenticate}
     get('/references', options).parsed_response
   end
 
   # Submit a request to create a new reference
-  def self.new_reference(amount, expiry_date)
-    post('/references', :body =>{:reference => {:amount      => amount,
-                                 :expiry_date => expiry_date}}.to_json,
-                        :basic_auth => {:username => ENV["PROXY_PAY_USER"], :password => ENV["PROXY_PAY_PASSWORD"]},
-                        :headers => { 'Content-Type' => 'application/json'}).parsed_response
+  def self.new_reference(amount, expiry_date, other_data={})
+    post('/references',
+      :body =>{ :reference => {:amount => amount, :expiry_date => expiry_date, :custom_fields => other_data } }.to_json,
+      :basic_auth => authenticate,
+      :headers => { 'Content-Type' => 'application/json'}).parsed_response
   end
 
   # Fetch all availables payments
   def self.get_payments(options={})
+    options = {:basic_auth => authenticate}
+    get('/events/payments', options).parsed_response
+  end
+
+  private
+  def self.authenticate
     auth = {
       username:ENV["PROXY_PAY_USER"],
       password:ENV["PROXY_PAY_PASSWORD"]
     }
-    options = {basic_auth:auth}
-    get('/events/payments', options).parsed_response
   end
 end
 
